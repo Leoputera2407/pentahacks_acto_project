dbd576fe5bb00b52736576cbf27c1811d091a173
Leoputera2407 <cookieavarice@gmail.com>
2018-11-21 01:19:44 -0800

    Speech-To-Text

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 const uuid = require('uuid');
+const speech = require('@google-cloud/speech');
+require("dotenv").config({ path: path.join(__dirname, "settings.env") });
+
 const FILE_UPLOAD_DIR= path.join(__dirname, "uploads");
 if (!fs.existsSync(FILE_UPLOAD_DIR)){
     console.log(FILE_UPLOAD_DIR, "does not exist. Creating it...");
@@ -61,3 +64,36 @@ app.post('/upload/pitch',cors(), function(req, res){
   };
 })
 
+async function transcribe(file) {
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The name of the audio file to transcribe
+  const fileName = `./uploads/${file}`;
+
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+
+}
