bcf159425988aa551ed4962205835638d6531993
willyspinner <wilsonjusuf1998@gmail.com>
2018-11-22 01:54:24 -0800

    restructuring openvokaturi and vokaturi works now.

--- backend/vokaturi.js
+++ backend/vokaturi.js
@@ -1,24 +1,35 @@
-const fs = require('fs');
+const path = require('path')
 const child_process = require("child_process");
-
+require("dotenv").config({ path: path.join(__dirname, "settings.env") });
 if ( !process.env.VOKATURI_EXECUTABLE){
     throw new Error ("VOKATURI_EXECUTABLE env var not defined.")
 }
 
-const VokaturiAnalyzeTone = async (absolute_audiopath) => {
-    const FILEPATH = absolute_audiopath;
+VOKATURI_EXECUTABLE = process.env.VOKATURI_EXECUTABLE;
+const VokaturiAnalyzeTone = (absolute_audiopath) => {
+    return new Promise((resolve,reject)=>{
+        const FILEPATH = absolute_audiopath;
 
-    var spawn = child_process.spawn; 
-    var process = spawn('python3', [process.env.VOKATURI_EXECUTABLE, FILEPATH]);
+        var spawn = child_process.spawn;
+        var process = spawn('python3', [VOKATURI_EXECUTABLE, FILEPATH],
+            {
+                cwd: path.dirname(VOKATURI_EXECUTABLE)
+            });
 
-    process.stderr.on('data', (data)=>{
-        console.error("FIND ERR TING", data.toString);
-    })
-
-    process.stdout.on('close', (result) => {
-        //TODO: Fire websocket
-        return (result);
-    })
+        process.stderr.on('data', (data)=>{
+            console.error("FIND ERR TING", data.toString);
+        })
+        let STDOUT = "";
+        process.stdout.on('data', (chunk) => {
+            STDOUT += chunk;
+        });
+        process.stdout.on('close', (err) => {
+            if (err){
+                reject(err);
+            }
+            resolve(STDOUT);
+        })
+    });
 }
 
 module.exports = VokaturiAnalyzeTone;
--- backend/workers.js
+++ backend/workers.js
@@ -2,9 +2,11 @@ const workerpool = require('workerpool');
 
 const {transcribe} = require('./gcp.js');
 const kairosAnalyzeVideoEmotion = require('./kairos');
+const VokaturiAnalyzeTone = require('./vokaturi');
 
 
 workerpool.worker({
     analyzeVideoEmotion : kairosAnalyzeVideoEmotion,
     transcribe : transcribe,
+    analyzeTone: VokaturiAnalyzeTone
 })
